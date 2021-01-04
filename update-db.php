faaad055e72814790e3ab42b20df245bc06285a5
Stephen De Ceuster <stephen@sidebury.com>
2021-01-04 18:48:01 +0100

    toevoeging update-db.php + enkele templates 🤓

---
+++ update-db.php
@@ -0,0 +1,12 @@
+<?php
+// display errors
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+// load library
+require_once './lib/autoload.php';
+?>
+
+//form
+
+// Hier komt update formulier om platen toe te voegen/wijzen in db
