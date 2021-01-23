2f8cdb966c8d138c903e8e8a4a062560bc5b5e31
Stephen De Ceuster <stephen@sidebury.com>
2021-01-23 22:34:55 +0100

    update form

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+//error_reporting( E_ALL );
+//ini_set( 'display_errors', 1 );
 
-error_reporting( E_ALL );
-ini_set( 'display_errors', 1 );
+$public_access = true;
 
 // Haalt de formules binnen
 require_once "lib/autoload.php";
--- lib/autoload.php
+++ lib/autoload.php
@@ -6,8 +6,8 @@ require_once "pdo.php";
 require_once "html_functions.php";
 require_once "form_elements.php";
 require_once "sanitize.php";
-require_once "validate.php";
 require_once "security.php";
+require_once "validate.php";
 
 $errors = [];
 
